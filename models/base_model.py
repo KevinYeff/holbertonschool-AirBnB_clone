ba2cae5fc76777d95eea6dda87f05ea5a6616e2d
AlExito745 <alxtinoco1988@gmail.com>
2023-02-21 09:04:47 -0800

    doc: update documentation style

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 # Yeff Espinoza <5153@holbertonstudents.com>
 """Define a class BaseModel to support other classes.
 """
+
 import uuid
 from datetime import datetime
 
@@ -15,6 +16,7 @@ class BaseModel:
     def __init__(self, id="", created_at=None, updated_at=None):
         """Constructor new instances of class.
         """
+        
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -22,17 +24,20 @@ class BaseModel:
     def __str__(self):
         """String representation of the instances.
         """
+        
         str = "[BaseModel] ({}) {}".format(self.id, self.__dict__)
         return(str)
 
     def save(self):
         """Updates the public instance attributes.
         """
+        
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values.
         """
+        
         n_dict = self.__dict__.copy()
         if "created_at" in n_dict:
             n_dict["created_at"] = n_dict["created_at"].isoformat()
