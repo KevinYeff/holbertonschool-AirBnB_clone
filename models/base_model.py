b7f3003488ff6c720a18a28c418a5c1652a9260b
AlExito745 <alxtinoco1988@gmail.com>
2023-02-21 08:58:05 -0800

    doc: fix documentation style

--- models/base_model.py
+++ models/base_model.py
@@ -2,31 +2,37 @@
 # base_model.py
 # Alex Nuñez <5694@holbertonstudents.com>
 # Yeff Espinoza <5153@holbertonstudents.com>
-"""Define a class BaseModel to support other classes."""
+"""Define a class BaseModel to support other classes.
+"""
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """Represent support to other classes."""
+    """Represent support to other classes.
+    """
 
     def __init__(self, id="", created_at=None, updated_at=None):
-        """Constructor new instances of class."""
+        """Constructor new instances of class.
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """String representation of the instances."""
+        """String representation of the instances.
+        """
         str = "[BaseModel] ({}) {}".format(self.id, self.__dict__)
         return(str)
 
     def save(self):
-        """Updates the public instance attributes."""
-        return(self.updated_at)
+        """Updates the public instance attributes.
+        """
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values."""
+        """Returns a dictionary containing all keys/values.
+        """
         n_dict = self.__dict__.copy()
         if "created_at" in n_dict:
             n_dict["created_at"] = n_dict["created_at"].isoformat()
