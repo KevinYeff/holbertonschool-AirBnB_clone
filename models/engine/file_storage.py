379e58529d057bb94ee5a29a51467b299e2016a4
KevinYeff <kevinyeff94@gmail.com>
2023-02-24 09:52:02 -0500

    fix: chaging reference to class.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,68 +5,41 @@
 """Define a class FileStorage that serializes instances.
 """
 
-
 import json
 import os
 
 
-class FileStorage():
-    """
-    Class that serializes instances to a JSON file and deserializes
-    JSON file to instances
-    
-    Private Class Attributes:
-        __file_path (str):
-            path to JSON file (ex: file.json)
-        __objects (dict):
-            empty dictionary that will store all objects. The format
-            will be as follows: <class name>.id (ex: to store a
-            BaseModel object with id=12121212, the key will be
-            BaseModel.id)
-            
-    Public Instance Methods:
-        all(self):
-            returns the dictionary __objects
-        new(self, obj):
-            sets in __objects the obj with key <obj class> name.id
-        save(self):
-            serializes __objects to the JSON file (path: __file_path)
-        reload(self):
-            deserializes the JSON file to __objects
+class FileStorage:
+    """Represent a file storage of instances.
     """
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """ Returns the dictionary __objects """
-        self.reload()
-        return self.__objects
+        """Return the dictionary of the class attribute(__objects).
+        """
+        return (self.__objects)
 
     def new(self, obj):
-        """ Sets in __objects the obj with key <obj class name>.id
-
-        Parameters:
-            obj (any):
-                object to be stored in __objects dictionary
+        """Sets in __objects the obj with key (<obj class name>.id).
         """
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj.to_dict()
+        k_obj = "{}.{}".format(type(obj).__name__, obj.id)
+        self.__objects[k_obj] = obj.to_dict()
 
     def save(self):
+        """Serializes __objects to the JSON file(path: __file_path).
         """
-        Serializes __objects dictionary to JSON string and stores in
-        __file_path
-        """
-        with open(self.__file_path, 'w', encoding='utf-8') as file:
-            json.dump(self.__objects, file)
+        f_in = self.__file_path
+        d = self.__objects
+        with open(f_in, "w", encoding="utf-8") as f_out:
+            json.dump(d, f_out)
 
     def reload(self):
-        """ Deserializes the JSON file to __objects
-        If the JSON file (__file_path) exists, deserializes the JSON
-        file to __objects dictionary. Otherwise, does nothing, and no exception
-        should be raised
+        """Deserializes the JSON file to __objects.
         """
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, 'r', encoding='utf-8') as file:
-                self.__objects = json.load(file)
+        f_in = self.__file_path
+        d = self.__objects
+        if os.path.exists(f_in):
+            with open(f_in, "r", encoding="utf-8") as f_out:
+                d = json.load(f_out)
