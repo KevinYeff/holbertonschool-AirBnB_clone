26f4d8b078f0e8db126727baf520e73054247134
AlExito745 <alxtinoco1988@gmail.com>
2023-02-23 18:24:54 -0800

    feat:change directory

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# test_file_storage.py
+# Alex Nuñez <5694@holbertonstudents.com>
+# Yeff Espinoza <5153@holbertonstudents.com>
+"""Test for class FileStorage."""
+
+
+import unittest
+import models
+
+FileStorage = models.file_storage.FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """Class for testing FileStorage docs."""
+
+    def test_doc_module(self):
+        """Test moduel documentation."""
+        actual = models.file_storage.__doc__
+        self.assertIsNotNone(actual)
+
+    def test_doc_class(self):
+        """Test for class documentation."""
+        actual = FileStorage.__doc__
+        self.assertIsNotNone(actual)
+
+    def test__file_path(self):
+        """Test for class private attribute __file_path."""
+        actual = FileStorage.__file_path
+        self.assertIsNone(actual)
+      
+if __name__ == '__main__':
+    unittest.main
