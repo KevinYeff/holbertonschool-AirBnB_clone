709be135227eb83f158e8db19ac2cbe65968a13e
AlExito745 <alxtinoco1988@gmail.com>
2023-02-23 17:19:33 -0800

    feat:update code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -50,7 +50,7 @@ class FileStorage():
             obj (any):
                 object to be stored in __objects dictionary
         """
-        key = "{}.{}".format(type(obj).__name__, obj.id)
+        key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj.to_dict()
 
     def save(self):
