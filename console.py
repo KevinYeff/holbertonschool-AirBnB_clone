6a7a895335a7a3274b6b84ad78cce6ac666bec80
KevinYeff <kevinyeff94@gmail.com>
2023-02-26 16:19:27 -0500

    feat: importing more clasess

--- console.py
+++ console.py
@@ -10,6 +10,11 @@ from models.base_model import BaseModel
 from models.__init__ import storage
 from models.__init__ import models_classes
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
