7f6331eb28a3241454857d7a1714713e9adc1db6
KevinYeff <kevinyeff94@gmail.com>
2023-02-26 16:25:36 -0500

    fix: city_id and user_id values

--- models/place.py
+++ models/place.py
@@ -11,8 +11,8 @@ class Place(BaseModel):
     Class that inherits from BaseModel and represents
     a place
     """
-    city_id = "City.id"
-    user_id = "User.id"
+    city_id = ""
+    user_id = ""
     name = ""
     description = ""
     number_rooms = 0
