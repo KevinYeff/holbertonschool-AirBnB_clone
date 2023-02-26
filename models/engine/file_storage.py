aff91a98ecbf8e6ffa2bc437839df55f5002a3e6
KevinYeff <kevinyeff94@gmail.com>
2023-02-26 12:02:12 -0500

    fix: changing some methos definitions

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,28 +20,27 @@ class FileStorage:
     def all(self):
         """Return the dictionary of the class attribute(__objects).
         """
-        self.reload()
-        return (self.__objects)
+        return (self.__class__.__objects)
 
     def new(self, obj):
         """Sets in __objects the obj with key (<obj class name>.id).
         """
         k_obj = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[k_obj] = obj
+        self.__class__.__objects[k_obj] = obj
 
     def save(self):
         """Serializes __objects to the JSON file(path: __file_path).
         """
-        f_in = self.__file_path
+        f_in = FileStorage.__file_path
         d = {}
-        for k, v in self.__objects.items():
+        for k, v in self.__class__.__objects.items():
             d[k] = v.to_dict()
         with open(f_in, "w", encoding="utf-8") as f_out:
             json.dump(d, f_out)
 
     def reload(self):
         """Deserializes the JSON file to __objects."""
-        f_in = self.__file_path
+        f_in = FileStorage.__file_path
         if os.path.exists(f_in):
             with open(f_in, "r", encoding="utf-8") as f_out:
                 obj_dict = json.load(f_out)
@@ -51,4 +50,4 @@ class FileStorage:
                     obj_class = models.models_classes[cls]
                     obj_instance = obj_class(**v)
                     objs_loaded["{}.{}".format(cls, k)] = obj_instance
-                self.__objects.update(objs_loaded)
+                self.__objects.update(**objs_loaded)
