878cbdedcad8e7b768c8e6766575bb571e750f84
KevinYeff <kevinyeff94@gmail.com>
2023-02-26 18:43:52 -0500

    feat: testing checker

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -12,9 +12,23 @@ from models.city import City
 class TestCity(unittest.TestCase):
     """Test suites for class State."""
 
+    def setUp(self):
+        """
+        Setup resources to be used in the tests
+        i) Create a City object
+        """
+        self.city = City()
+
+    def tearDown(self):
+        """
+        Clean up resources after the tests
+        i) Delete the instance created
+        """
+        del self.city
+
     def test_doc_module(self):
         """Test module documentation."""
-        actual = models.city.__doc__
+        actual = City.__module__.__doc__
         self.assertIsNotNone(actual)
 
     def test_doc_class(self):
@@ -31,6 +45,7 @@ class TestCity(unittest.TestCase):
         """Test that object attributes are the same type."""
         self.assertTrue(type(self.city.name) == str)
         self.assertTrue(type(self.city.state_id) == str)
-      
+
+
 if __name__ == '__main__':
     unittest.main
